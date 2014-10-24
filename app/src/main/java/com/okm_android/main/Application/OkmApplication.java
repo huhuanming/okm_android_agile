edc885f4a182308d14fc35447b6ae2640171ddb9
chenhong <924010725@qq.com>
2014-10-24 15:09:29 +0800

    上传

--- app/src/main/java/com/okm_android/main/Application/OkmApplication.java
+++ app/src/main/java/com/okm_android/main/Application/OkmApplication.java
@@ -4,6 +4,8 @@ import android.app.Application;
 
 import com.activeandroid.ActiveAndroid;
 
+import cn.jpush.android.api.JPushInterface;
+
 /**
  * Created by hu on 14-10-18.
  */
@@ -11,7 +13,10 @@ public class OkmApplication extends Application {
     @Override
     public void onCreate() {
         super.onCreate();
+        JPushInterface.setDebugMode(true); 	// 设置开启日志,发布时请关闭日志
+        JPushInterface.init(this);     		// 初始化 JPush
         ActiveAndroid.initialize(this);
+
     }
     @Override
     public void onTerminate() {
